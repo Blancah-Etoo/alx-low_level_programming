323ffb2947571ae3211d339a74280a79e3340977
Blancah_Etoo <etoo659@gmail.com>
2023-03-17 02:40:40 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - this is the main function
+ * main - Entry point
  *
- * Return:  always 0
+ * Return: always 0
  */
- 
-int main(void)
-{
+int main(void) 
+{	
         puts("\"Programming is like building a multilingual puzzle");
         return (0);
-
 }	
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stduo.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{	printf("Size of a char: %lu bytes(s)", sizeof(char));
+	printf("Size of a int: %lu bytes(s)", sizeof(int));
+	printf("Size of a long int: %lu bytes(s)", sizeof(long int));
+	printf("Size of a long long int: %lu bytes(s)", sizeof(long long  int));
+	printf("Size of a float: %lu bytes(s)", sizeof(float));
+	return (0):
+		
+}
