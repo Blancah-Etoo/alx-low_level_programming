4f850765ddc90be1252ca2110154eb54e53bdc60
Blancah_Etoo <etoo659@gmail.com>
2023-03-22 03:13:15 -0700

    alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,12 @@
 #include "main.h"
+
 /**
  * print_alphabet - function that prints alphabet in lower case
  *
  * Return: always 0
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char i;
 
