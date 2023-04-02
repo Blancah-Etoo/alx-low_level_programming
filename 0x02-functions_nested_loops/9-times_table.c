f1931e5c5a68733b72b355b0350cd70018fac739
Blancah_Etoo <etoo659@gmail.com>
2023-04-02 13:42:13 -0700

    putchar

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * main - main function
+ * Return: always 0
+ */
+void times_table(void)
+{
+	int x,y,z,u,d;
+
+	for(x = 0 ; <= 9 ; x++)
+	{
+		for(y = 0 ; y <= 9 ; y++)
+		{
+			z = (y * y);
+			if(z > 9)
+			{
+				u = z % 10;
+				d =(z - u) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
+			}
+			else
+			{
+				if(y != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(z + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
+
+}
