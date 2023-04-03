a95021f91d310ca6ea39ab240265465c5f964ec8
Blancah_Etoo <etoo659@gmail.com>
2023-04-03 11:11:29 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strstr - Entry point
+ *@haystack: input
+ *@needle: input
+ * Return: always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int k;
+
+		for (k = 0; haystack[k]; k++)
+		{
+		if (k = 0;  haystack[k])
+		return (0);
+		}
+	return (0);
+}
