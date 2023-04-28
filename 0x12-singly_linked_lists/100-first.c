b2ee53bca3b607f564e76c8f48009a5437c53e26
Blancah_Etoo <etoo659@gmail.com>
2023-04-28 14:11:51 -0700

    hare

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-void first(void)_attribute_((constructor));
+void first(void) _attribute_ ((constructor));
+
 /**
  * first - prints a sentence before the main
  * function is excuted
  */
 void first(void)
-{
-printf("you are the best!, you must allow,\n");
+{	
+printf("you are the best! and yet, you must allow,\n")
 printf("I bore my house upon my back!\n");
 }
